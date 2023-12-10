9c572076fd50a4dd0e938bfab3054c7eda071a0e
hedykim <waltzbox@gmail.com>
2023-12-10 03:35:17 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/ACME-TF-WORKSHOP/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "gaurav-s3-bucket"
+  bucket_prefix = "gaurav-s3-bucket"
   acl    = "private"
 #    bucket_prefix = var.prefix
   versioning = {
